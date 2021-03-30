d5ef90debf0921be6f43334802f1f529970d69dc
yamidevs <>
2021-03-31 01:48:59 +0200

    Add Bitfinex exchange

---
+++ Nodes/Bitfinex/BitfinexConnectorNode.cs
@@ -0,0 +1,70 @@
+﻿using Bitfinex.Net;
+using Bitfinex.Net.Objects;
+using Bittrex.Net;
+using Bittrex.Net.Objects;
+using NodeBlock.Engine;
+using NodeBlock.Engine.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace NodeBlock.Plugin.Exchange.Nodes.Bitfinex
+{
+    [NodeDefinition("BitfinexConnectorNode","Bitfinex Connector",NodeTypeEnum.Connector, "Bitfinex")]
+    [NodeGraphDescription("Bitfinex Connector, for retrieve your ApiKey and ApiSecret go to your Bitfinex account and create them")]
+    public class BitfinexConnectorNode : Node
+    {
+        public BitfinexConnectorNode(string id,BlockGraph graph)
+            : base(id, graph, typeof(BitfinexConnectorNode).Name)
+        {
+            this.CanBeSerialized = false;
+
+
+            this.InParameters.Add("apiKey", new NodeParameter(this, "apiKey", typeof(string), true));
+            this.InParameters.Add("apiSecret", new NodeParameter(this, "apiSecret", typeof(string), true));
+
+            this.OutParameters.Add("bitfinex", new NodeParameter(this, "bitfinex", typeof(BitfinexConnectorNode), true));
+        }
+
+
+        [JsonIgnore]
+        public BitfinexClient Client { get; set; }
+
+
+        [JsonIgnore]
+        public BitfinexSocketClient SocketClient { get; set; }
+        public override bool CanBeExecuted => false;
+
+        public override bool CanExecute => true;
+
+        public override void SetupConnector()
+        {
+            this.Client = new BitfinexClient(new BitfinexClientOptions()
+            {
+                ApiCredentials = new CryptoExchange.Net.Authentication.ApiCredentials(this.InParameters["apiKey"].GetValue().ToString(), this.InParameters["apiSecret"].GetValue().ToString())
+            });
+
+            this.SocketClient = new BitfinexSocketClient();
+
+            this.Next();
+        }
+
+        public override void OnStop()
+        {
+            this.Client.Dispose();
+            this.SocketClient.Dispose();
+        }
+
+
+        public override object ComputeParameterValue(NodeParameter parameter, object value)
+        {
+            if (parameter.Name == "bitfinex")
+            {
+                return this;
+            }
+            return base.ComputeParameterValue(parameter, value);
+        }
+
+    }
+}
---
+++ Nodes/Bitfinex/OnBitfinexTickerUpdateNode.cs
@@ -0,0 +1,50 @@
+﻿using Bitfinex.Net.Objects;
+using NodeBlock.Engine;
+using NodeBlock.Engine.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace NodeBlock.Plugin.Exchange.Nodes.Bitfinex
+{
+    [NodeDefinition("OnBitfinexTickerUpdateNode", "On Bitfinex Pair Ticker Update", NodeTypeEnum.Event, "Bitfinex")]
+    [NodeGraphDescription("Get a event when the book ticker is updated on Bitfinex for a symbol")]
+    [NodeCycleLimit(1000)]
+    public class OnBitfinexTickerUpdateNode : Node
+    {
+        public OnBitfinexTickerUpdateNode(string id, BlockGraph graph)
+              : base(id, graph, typeof(OnBitfinexTickerUpdateNode).Name)
+        {
+            this.IsEventNode = true;
+
+            this.InParameters.Add("bitfinex", new NodeParameter(this, "bitfinex", typeof(BitfinexConnectorNode), true));
+            this.InParameters.Add("symbol", new NodeParameter(this, "symbol", typeof(string), true));
+
+ 
+            this.OutParameters.Add("bestPrice", new NodeParameter(this, "bestPrice", typeof(decimal), false));
+            this.OutParameters.Add("bestQuantity", new NodeParameter(this, "bestQuantity", typeof(decimal), false));
+        }
+
+        public override bool CanBeExecuted => false;
+
+        public override bool CanExecute => true;
+
+        public override void SetupEvent()
+        {
+            BitfinexConnectorNode binanceConnector = this.InParameters["bitfinex"].GetValue() as BitfinexConnectorNode;
+            binanceConnector.SocketClient.SubscribeToBookUpdates(this.InParameters["symbol"].GetValue().ToString(),Precision.PrecisionLevel0,Frequency.Realtime,100, (data) =>
+            {
+                var instanciatedParameters = this.InstanciateParametersForCycle();
+                instanciatedParameters["bestPrice"].SetValue((decimal)data.ToList()[0].Price);
+                instanciatedParameters["bestQuantity"].SetValue((decimal)data.ToList()[0].Quantity);
+                this.Graph.AddCycle(this, instanciatedParameters);
+            });
+        }
+
+        public override void BeginCycle()
+        {
+            this.Next();
+        }
+    }
+}
